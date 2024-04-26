faa841b651bbc213be1a35b1bb282a8b5c12e6a2
mustafa sen <mustafasen@tuta.io>
2024-04-26 05:16:29 +0300

    cleaned up the code

--- public/script/index.ts
+++ public/script/index.ts
@@ -1,11 +1,27 @@
 import L from "leaflet";
+import ws_client from "./ws";
+import { getMapBoundCoordinates } from "./utils";
+import { Markers } from "./markers";
 
-// Initialize the map
-var map = L.map("map").setView([51.505, -0.09], 13);
+const map = L.map("map").setView([51.505, -0.09], 13);
+const markers = new Markers(map);
 
-// Add OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+ws_client.onmessage = (msg) => {
+  const data = JSON.parse(msg.data);
+  if (!data.error) {
+    markers.setMarker(data);
+  } else {
+    console.log(Error(data.error));
+  }
+};
+
+map.on("moveend", () => {
+  const { swLat, swLng, neLat, neLng } = getMapBoundCoordinates(map);
+  const message = `[[[${swLat},${swLng}],[${neLat},${neLng}]]]`;
+  ws_client.send(message);
+});
